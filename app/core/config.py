7cbc2c8cdd1f3f42208ea1d0032e3cbaa1029a95
gmoroz <12345hasek@gmail.com>
2023-07-02 05:00:39 +0300

    refactor: config

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,52 @@
-# config.py
-
 import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
 
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
 
-DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
+@dataclass
+class DatabaseConfig:
+    db: str
+    user: str
+    password: str
+    server: str
+
+    @property
+    def url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.user}:{self.password}@{self.server}/{self.db}"
+        )
+
+
+@dataclass
+class RedisConfig:
+    host: str
+
+
+@dataclass
+class Env:
+    database: DatabaseConfig
+    redis: RedisConfig
+
+    @property
+    def database_url(self) -> str:
+        return self.database.url
+
+
+def load_config():
+    load_dotenv()
+
+    database_config = DatabaseConfig(
+        db=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        server=os.getenv("POSTGRES_SERVER"),
+    )
+
+    redis_config = RedisConfig(
+        host=os.getenv("REDIS_HOST"),
+    )
+
+    return Env(database=database_config, redis=redis_config)
+
+
+env = load_config()
